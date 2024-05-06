9e759300c1f655dfe0bf46add9f3c604dfc7b902
Florian CHAZAL <florian.chazal@doriane.com>
2024-05-06 12:57:34 +0200

    ci: workaround publish bug

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,7 +2,7 @@ import { awscdk } from 'projen';
 import { NpmAccess } from 'projen/lib/javascript';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Florian CHAZAL',
-  authorAddress: 'florian.chazal@doriane.com',
+  authorAddress: 'florianchazal@gmail.com',
   cdkVersion: '2.50.0',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.4.0',
@@ -10,6 +10,9 @@ const project = new awscdk.AwsCdkConstructLibrary({
   projenrcTs: true,
   repositoryUrl: 'https://github.com/flochaz/aws-evidently-l2.git',
   npmAccess: NpmAccess.PUBLIC,
+
+  // TODO: remove when https://github.com/projen/projen/issues/3479 is fixed
+  npmProvenance: false,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
